387fb98a29c98a75c3777ebae405983cd7eb6c3d
Gustavo René <renegustavo42@gmail.com>
2023-08-04 11:10:29 -0300

    feat: login implemented

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
 import db from './src/models';
 import UserController from './src/controllers/user';
 const app = express();
 
 app.use(express.json());
+
 app.post('/', (req, res) => {
     return UserController.create(req, res)
 });
+app.post('/login', UserController.login);
 
 app.listen(3333, async() => {
     try {
--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,24 +1,70 @@
 import { User } from "../models";
 import * as Yup from 'yup';
+import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 
 class UserController {
+    async login(req, res) {
+        try {
+            const schema = Yup.object().shape({
+                email: Yup.string().email('e-mail invalid.').required('e-mail is mandatory.'),
+                password: Yup.string().required('password is mandatory.'),
+            });
+
+            await schema.validate(req.body);
+
+            const user = await User.findOne({ 
+                where: {email: req.body.email} 
+            });
+
+            if(!user) {
+                return res.status(400).json({ error: 'e-mail or password do not match.' });
+            }
+
+            const checkPassword = await bcrypt.compare(req.body.password, user.password_hash);
+
+            if(!checkPassword) {
+                return res.status(401).json({ error: 'e-mail or password do not match.' });
+            }
+
+            const token = jwt.sign({ id: user.id }, process.env.JWT_HASH, {
+                expiresIn: '30d',
+            });
+
+            const { id, name, email, avatar_url, createdAt } = user;
+
+            return res.json({
+                id, 
+                name, 
+                email,
+                avatar_url, 
+                createdAt,
+                token,
+            });
+
+        } catch (error) {
+            return res.status(400).json({ error: error?.message });
+        }
+    }
+
     async create(req, res) {
         try {
             const schema = Yup.object().shape({
                 name: Yup.string().required('name is mandatory.').min(3, 'name must contain at least 3 characters.'),
                 email: Yup.string().email('e-mail invalid.').required('e-mail is mandatory.'),
-                password: Yup.string().required('password is mandatory.').min(6, 'password must contain at least 3 characters.')
+                password: Yup.string().required('password is mandatory.').min(6, 'password must contain at least 3 characters.'),
             });
+
+            await schema.validate(req.body);
     
-            const existedUser = await User.findOne({ where: { email: req.body.email } });
+            const existedUser = await User.findOne({ 
+                where: { email: req.body.email } 
+            });
 
             if(existedUser) {
                 return res.status(400).json({ error: 'user already exists.'});
             }
 
-            await schema.validate(req.body);
-    
             const hashPassword = await bcrypt.hash(req.body.password, 8);
     
             const user = new User({
